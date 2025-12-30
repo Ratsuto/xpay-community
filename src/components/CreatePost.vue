210708f6a3687cd7429095355a3cfc14f0f1612c
Ratsuto <sopharatt007@gmail.com>
2025-12-30 16:37:42 +0700

    Initial Commit 2

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="bg-white rounded-xl shadow p-4">
     <div class="flex gap-3">
-      <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/40"/>
-      <input
-          type="text"
-          placeholder="Share your thoughts..."
-          class="flex-1 bg-gray-100 rounded-full px-4 text-sm outline-none"
-      />
+      <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/40" alt="profile picture"/>
+      <input type="text" placeholder="Share your thoughts..." class="flex-1 bg-gray-100 rounded-full px-4 text-sm outline-none"/>
     </div>
 
     <div class="flex justify-between mt-4 text-sm text-gray-600">
